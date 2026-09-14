9ad9bb41110843a407b18b721ee4b9bc03944509
kaafisa <kaafisa770@gmail.com>
2026-09-14 15:27:53 +0300

    feat: save sidebar collapse state to local storage

--- main.js
+++ main.js
@@ -8,6 +8,45 @@ const sidebar=document.querySelector('.sidebar')
 
 collapsebtn.addEventListener('click',()=>{
   sidebar.classList.toggle('collapsed');
+
+  localStorage.setItem('collapsed',sidebar.classList.contains('collapsed'))
 })
 
+if(localStorage.getItem('collapsed')=== 'true'){
+    sidebar.classList.add('collapsed')
+}
 
+
+
+//adding themes and localStorage memory
+
+// light.addEventListener('click',()=>{
+//     document.body.classList.remove('night-theme','dark-theme')
+//     document.body.classList.add('light-theme')
+
+//     localStorage.setItem('theme','light')
+// })
+// night.addEventListener('click',()=>{
+//     document.body.classList.remove('light-theme','dark-theme')
+//     document.body.classList.add('night-theme')
+
+//     localStorage.setItem('theme','night')
+// })
+// dark.addEventListener('click',()=>{
+//     document.body.classList.remove('light-theme','night-theme')
+//     document.body.classList.add('dark-theme')
+
+//     localStorage.setItem('theme','dark')
+// })
+
+// //restoring the theme
+// const savedTheme= localStorage.getItem('theme')
+// if(savedTheme ==='light'){
+//     document.body.classList.add('light-theme')
+// }
+// if(savedTheme ==='night'){
+//     document.body.classList.add('night-theme')
+// }
+// if(savedTheme === 'light'){
+//     document.body.classList.add('light-theme')
+// }
