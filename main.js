04f37721fd8a9fd91fae6f8daa6f38a7154bec7e
kaafisa <kaafisa770@gmail.com>
2026-09-14 15:39:07 +0300

    feat: persist theme preferences with local storage

--- main.js
+++ main.js
@@ -20,33 +20,33 @@ if(localStorage.getItem('collapsed')=== 'true'){
 
 //adding themes and localStorage memory
 
-// light.addEventListener('click',()=>{
-//     document.body.classList.remove('night-theme','dark-theme')
-//     document.body.classList.add('light-theme')
+light.addEventListener('click',()=>{
+    document.body.classList.remove('night-theme','dark-theme')
+    document.body.classList.add('light-theme')
 
-//     localStorage.setItem('theme','light')
-// })
-// night.addEventListener('click',()=>{
-//     document.body.classList.remove('light-theme','dark-theme')
-//     document.body.classList.add('night-theme')
+    localStorage.setItem('theme','light')
+})
+night.addEventListener('click',()=>{
+    document.body.classList.remove('light-theme','dark-theme')
+    document.body.classList.add('night-theme')
 
-//     localStorage.setItem('theme','night')
-// })
-// dark.addEventListener('click',()=>{
-//     document.body.classList.remove('light-theme','night-theme')
-//     document.body.classList.add('dark-theme')
+    localStorage.setItem('theme','night')
+})
+dark.addEventListener('click',()=>{
+    document.body.classList.remove('light-theme','night-theme')
+    document.body.classList.add('dark-theme')
 
-//     localStorage.setItem('theme','dark')
-// })
+    localStorage.setItem('theme','dark')
+})
 
-// //restoring the theme
-// const savedTheme= localStorage.getItem('theme')
-// if(savedTheme ==='light'){
-//     document.body.classList.add('light-theme')
-// }
-// if(savedTheme ==='night'){
-//     document.body.classList.add('night-theme')
-// }
-// if(savedTheme === 'light'){
-//     document.body.classList.add('light-theme')
-// }
+//restoring the theme
+const savedTheme= localStorage.getItem('theme')
+if(savedTheme ==='light'){
+    document.body.classList.add('light-theme')
+}
+if(savedTheme ==='night'){
+    document.body.classList.add('night-theme')
+}
+if(savedTheme === 'light'){
+    document.body.classList.add('light-theme')
+}
